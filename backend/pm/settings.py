cfa8714d114f2ccf9e06553c3f7ebe4e23479e79
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2024-09-10 00:03:31 +0600

    'cors_headers_add'

--- backend/pm/settings.py
+++ backend/pm/settings.py
@@ -23,11 +23,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +57,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pm.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
